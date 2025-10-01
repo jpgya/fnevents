661b59d1927c4fed7e1b274179b95faee9eef3aa
Rion <kanade.miyachi@gmail.com>
2025-10-01 19:27:17 +0900

    electronの定義

--- .Window/main.js
+++ .Window/main.js
@@ -1,2 +1,16 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), 
+      contextIsolation: true, 
+      nodeIntegration: false 
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+}
